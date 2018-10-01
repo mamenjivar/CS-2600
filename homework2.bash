7794cf66a7112240101b3b7c556f5321838e1cff
mamenjivar <Miguel@b98-aruba-authenticated-10-110-159-80.wlan.cpp.edu>
2018-10-01 14:46:19 -0700

    updating file which is finished

--- homework2.bash
+++ homework2.bash
@@ -3,6 +3,7 @@
 # HW 02
 # CS 2600
 
+# function provided by professor to read different languages
 function comchar() {
     case "$1" in
        java|cpp|c)
@@ -23,15 +24,20 @@ COMMENT_CHARACTER=$(comchar $2)
 line_count=$(wc -l $1 | cut -d' ' -f1)
 
 # gets total number of lines that are comments
-comment_count=$(grep -c $COMMENT_CHARACTER $1)
+comment_count=$(grep -c ^$COMMENT_CHARACTER $1)
 
 # gets total number of lines that are blank
 blank_lines=$(grep -c '^\s*$' $1)
 
+# gets total number of lines that are only braces
+braces=$(grep -c '^[{}]' $1)
+
+# gets the total number of lines that is actual code
+actual_code=$(($line_count-$comment_count-$blank_lines-$braces))
+
+# Prints values to screen
 echo "Total Lines  : $line_count"
 echo "Comments     : $comment_count"
 echo "Blank lines  : $blank_lines"
-echo "Only Braces  :"
-echo "Actual Code  :"
-echo $1
-echo $2
+echo "Only Braces  : $braces"
+echo "Actual Code  : $actual_code"
